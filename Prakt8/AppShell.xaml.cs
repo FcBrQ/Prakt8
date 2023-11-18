97e063ffa5a184559c0d09209105ee3caa190e88
Altari <zyw.err@mail.ru>
2023-11-18 21:54:00 +0300

    Добавьте файлы проекта.

---
+++ Prakt8/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Prakt8
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Prakt8/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace Prakt8
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        string manDir = FileSystem.Current.AppDataDirectory;
+        private void BornDP_DateSelected(object sender, DateChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync();
+            if(result != null) {
+            var selectedPhotoStream = await result.OpenReadAsync();
+                ImageView.Source = ImageSource.FromStream(() => selectedPhotoStream);
+            }
+        }
+    }
+}
